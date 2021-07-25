afa3393fb8895fa560c8e0c6eaa7d254d1d9b427
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-07-25 15:26:23 +0200

    Convert Main.js to functional component on hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Route,
   HashRouter,
@@ -6,118 +6,114 @@ import {
 } from "react-router-dom";
 import Profile from "./Profile";
 import Products from "./Products";
-import UserApi from '../api/UserApi.js';
 import CalendarComp from './CalendarComp';
 import CalendarDay from './CalendarDay';
 import Nav from './Nav';
 import '../css/Main.css';
-import TokenApi from '../api/TokenApi.js';
 
+import userService from '../api/services/user';
+import tokenService from '../api/services/token';
 
-class Main extends Component {
+const Main = () => {
+  const [user, setUser] = useState({
+    username: "",
+    avatar: null,
+    currentWeight: "",
+    desiredWeight: "",
+    date: 0,
+    mounted: false,
+  })
 
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      avatar: null,
-      currentWeight: "",
-      desiredWeight: "",
-      date: 0,
-      mounted: false
-    };
-  }
-
-  componentDidMount() {
-    UserApi.user.getUser()
-      .then(user => {
-        this.setState({
-          username: user.username,
-          avatar: user.avatar,
-          currentWeight: user.currentWeight,
-          desiredWeight: user.desiredWeight
-        });
-      })
-      this.setState({
+  useEffect(() => {
+    const fetch = async() => {
+      const fetchedUser = await userService.getUser();
+      setUser(fetchedUser);
+    }
+    fetch();
+    setUser(old => {
+      return {...old, ...{
         mounted: true
-      })
-    this.interval = setInterval(this.refresh, this.minutesToMili(2) - 10)
-  }
+      }}
+    });
+    const interval = setInterval(refresh, minutesToMili(2) - 10)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
-  componentWillUnmount(){
-    clearInterval(this.interval)
-  }
+  const minutesToMili = minutes => minutes * 1000 * 60;
 
-  minutesToMili = minutes => minutes * 1000 * 60
-
-  changeInfo = callback => {
-    this.setState({
-      username: callback.username,
-      currentWeight: callback.currentWeight,
-      desiredWeight: callback.desiredWeight
+  const changeInfo = callback => {
+    setUser(old => {
+      return {
+        ...old, ...{
+          username: callback.username,
+          currentWeight: callback.currentWeight,
+          desiredWeight: callback.desiredWeight
+        }
+      }
     })
   }
-  renderDay = (month, day, year) => {
-    this.setState({
-      date: day + month + year
+  const renderDay = (month, day, year) => {
+    setUser(old => {
+      return {
+        ...old, ...{
+            date: day + month + year
+        }
+      }
     })
   }
-  refresh = () => {
+  const refresh = async() => {
     const refresh = JSON.stringify({
       refresh: localStorage.getItem('refresh_token')
     })
-    TokenApi.token.refresh(refresh)
-      .then(res => {
-        if (res !== null) {
-          localStorage.setItem('access_token', res.access);
-          localStorage.setItem('refresh_token', res.refresh);
-        }
-      })
+
+    const res = await tokenService.refresh(refresh);
+    if(res){
+      localStorage.setItem('access_token', res.access);
+      localStorage.setItem('refresh_token', res.refresh);
       alert("Token refreshed")
+    }
   }
-  render() {
-    if(this.state.mounted){
-      return (
-        <div className="Main">
-          <HashRouter>
-            <Redirect to="/home" />
-            <div className="wrapper">
-              <div className="side_container">
-                <div className="info_container">
-                  <img className="info_item" src={this.state.avatar} alt="avatar" height="90" width="90" />
-                  <h4 className="info_item">{this.state.username}</h4>
-                  <div className="weight_wrapper">
-                    <h5 className="info_item">{this.state.currentWeight}</h5>
-                    <h5 className="info_item">{this.state.desiredWeight}</h5>
-                  </div>
+
+  if(!user.mounted)
+    return null;
+
+    return (
+      <div className="Main">
+        <HashRouter>
+          <Redirect to="/home" />
+          <div className="wrapper">
+            <div className="side_container">
+              <div className="info_container">
+                <img className="info_item" src={user.avatar} alt="avatar" height="90" width="90" />
+                <h4 className="info_item">{user.username}</h4>
+                <div className="weight_wrapper">
+                  <h5 className="info_item">{user.currentWeight}</h5>
+                  <h5 className="info_item">{user.desiredWeight}</h5>
                 </div>
-                <Nav />
               </div>
-              <div className="main_container">
-                <Route
-                  path="/profile"
-                  render={(props) => <Profile {...props} handleChange={this.changeInfo} />}
-                />
-                <Route
-                  path="/calendar"
-                  render={(props) => <CalendarComp {...props} getDay={this.renderDay} />}
-                />
-                <Route path="/saved" component={Products} />
-                <Route
-                  path={'/' + this.state.date}
-                  render={(props) => <CalendarDay {...props} date={this.state.date} />}
-                />
-              </div>
+              <Nav />
             </div>
-          </HashRouter>
-        </div>
-      );
-    }
-    else{
-      return null;
-    }
-  }
+            <div className="main_container">
+              <Route
+                path="/profile"
+                render={(props) => <Profile {...props} handleChange={changeInfo} />}
+              />
+              <Route
+                path="/calendar"
+                render={(props) => <CalendarComp {...props} getDay={renderDay} />}
+              />
+              <Route path="/saved" component={Products} />
+              <Route
+                path={'/' + user.date}
+                render={(props) => <CalendarDay {...props} date={user.date} />}
+              />
+            </div>
+          </div>
+        </HashRouter>
+      </div>
+    );
 }
 
 export default Main;
-
